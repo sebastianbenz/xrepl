ac71598df2ddda9ba587a67bb77cbe90d061aca3
Sebastian Benz <sebastian.benz@gmail.com>
2011-02-05 21:59:29 +0100

    added copyright headers

--- org.xrepl.xscript/src/org/xrepl/xscript/scoping/EPackageScopeProvider.java
+++ org.xrepl.xscript/src/org/xrepl/xscript/scoping/EPackageScopeProvider.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2002-2006 Sebastian Benz and others.
+ * Copyright (c) 2002, 2011 Sebastian Benz and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
